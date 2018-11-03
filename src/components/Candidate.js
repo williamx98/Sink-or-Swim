bac1353c568a5b40f7aefd5f12f9fe62c996c62a
Kathleen Doviken <kathleendoviken@gmail.com>
2018-11-03 12:28:37 -0500

    start

---
+++ src/components/Candidate.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+class Candidate extends Component {
+    render() {
+        return (
+            <div className="candidate">
+                
+            </div>
+        )
+    }
+}
+
+export default Candidate;
