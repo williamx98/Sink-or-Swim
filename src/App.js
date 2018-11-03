8c6908f7ceaf93f5c351f59e4cd18e12c00fe703
Will Xu <willyamxu@gamil.com>
2018-11-03 14:12:42 -0500

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import CandidatePage from './CandidatePage'
 import './App.css';
 
 var Parse = require('parse/node');
 
-const Index = () => <h2>Home</h2>;
-const About = () => <h2>About</h2>;
-const Users = () => <h2>Users</h2>;
-
+const Candidate = () => <CandidatePage/>;
+const Users = () => <h2>Topics</h2>;
+const Index = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      {/* Change this link to set query string*/}
+      <Link to="/candidate?id=1234">Candidate ID: 1234</Link>
+    </li>
+  </ul>
+);
 
 class App extends Component {
   initializeParse() {
@@ -42,7 +50,6 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.initializeParse();
-    this.logIn("username","password");
   }
 
   componentDidMount() {
@@ -51,44 +58,13 @@ class App extends Component {
 
   render() {
     return (
-      /*<div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>*/
       <Router>
         <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about/">About</Link>
-              </li>
-              <li>
-                <Link to="/users/">Users</Link>
-              </li>
-            </ul>
-          </nav>
-
-          <Route path="/" exact component={Index} />
-          <Route path="/about/" component={About} />
-          <Route path="/users/" component={Users} />
+          <Route exact path="/" component={Index} />
+          <Route exact path="/candidate" component={Candidate} />
+          <Route exact path="/topics" component={Users} />
         </div>
       </Router>
-
     );
   }
 }
