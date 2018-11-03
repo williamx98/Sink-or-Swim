b2742db3a51e91a2a9b9f532cbe56ec5a242c6b9
Will Xu <willyamxu@gamil.com>
2018-11-03 11:13:25 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var Parse = require('parse/node');
+
+
+
 class App extends Component {
+  async makeUser(username, password) {
+    var user = new Parse.User();
+    user.set("username", username);
+    user.set("password", password);
+
+    try {
+      await user.signUp();
+      console.log("Signed up AND logged in");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async logIn(username, password) {
+    try {
+      await Parse.User.logIn(username, password);
+      console.log("logged in successful");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    Parse.initialize("moodyhack2018", "myMasterKey");
+    Parse.serverURL = "https://moodyhack2018.herokuapp.com/parse";
+    this.logIn("asdf","asdf");
+  }
+
+  componentDidMount() {
+    console.log("mounted")
+  }
+
   render() {
     return (
       <div className="App">
@@ -24,5 +61,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
